4fa6bf77ab215693033b51d03ad81efba579c909
knownProgramisT <yuraslobodyan12@gmail.com>
2021-04-14 21:26:15 +0300

    PW-6

---
+++ PW-6/main.php
@@ -0,0 +1,17 @@
+﻿<?php 
+
+$title = $_POST['title'];
+$content = $_POST['content'];
+
+mysqli_report( MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT );
+$connection = mysqli_connect('localhost', 'root1', 'root1', 'ajax');
+if(!$connection){
+    die("Error: No connection with MySQL.");
+}
+
+$connection->query("INSERT INTO `posts` (`title`, `content`) VALUES ('$title', '$content') ");
+$connection->close();
+
+echo("<script>location.href = '"."/index.html?msg=$msg';</script>");
+
+?>
---
+++ PW-6/script.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function(){
+    $('button').on('click', function(){
+        let titleValue = $('input').val();
+        let contentValue = $('textarea').val();
+
+        $.ajax({
+            method: "POST",
+            url: "main.php",
+            data: { title: titleValue, content: contentValue }
+        })
+        .done(function(){
+            $('input').val('');
+            $('textarea').val('');   
+            alert('Data sent!'); 
+        });
+    })
+});
+
