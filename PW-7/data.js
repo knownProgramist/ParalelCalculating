3f75140b1a084a8a2513df463a46e2131de14de5
knownProgramisT <yuraslobodyan12@gmail.com>
2021-05-12 13:51:12 +0300

    PW-7

---
+++ PW-7/data.js
@@ -0,0 +1,11 @@
+getUserName = document.querySelector(".userName");
+getUserAge = document.querySelector(".userAge");
+getButton = document.querySelector(".submit");
+
+getButton.addEventListener('click', function(){
+  getNameDisplay = document.querySelector(".displayName");
+  getAgeDisplay = document.querySelector(".displayAge");
+
+  getNameDisplay.innerHTML += getUserName.value;
+  getAgeDisplay.innerHTML += getUserAge.value;
+})
